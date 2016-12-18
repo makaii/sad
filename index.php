b962de9e77c998da2cc01b9d81d9edfb3a995b30
makaii <markjoshuaevangelista@gmail.com>
2016-12-18 10:36:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Wealt Management</title>
+
+	<!-- CSS' and SCRIPTS' -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+	<script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+
+	<!-- styles -->
+
+
+</head>
+<body>
+
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+		
+		<header class="mdl-layout__header">
+			<div class="mdl-layout__header-row">
+				<span class="mdl-layout-title">Wealth Management</span>
+				<div class="mdl-layout-spacer"></div>
+				<nav class="mdl-navigation  mdl-layout--large-screen-only">
+					<a class="mdl-navigation__link" href="signin.php" target="_blank">Sign In</a>
+					<a class="mdl-navigation__link" href="signup.php" target="_blank">Sign Up</a>
+					<a class="mdl-navigation__link" href="aboutus.php">About Us</a>
+				</nav>
+				</nav>
+			</div>
+		</header>
+
+		<div class="mdl-layout__drawer mdl-layout--small-screen-only">
+			<span class="mdl-layout-title">Wealth Management</span>
+			<nav class="mdl-navigation">
+				<a class="mdl-navigation__link" href="signin.php" target="_blank">Sign In</a>
+				<a class="mdl-navigation__link" href="signup.php" target="_blank">Sign Up</a>
+				<a class="mdl-navigation__link" href="aboutus.php">About Us</a>
+			</nav>
+		</div>
+
+		<!-- main content -->
+		<main class="mdl-layout__content">
+			<div class="page-content">
+				<div class="mdl-grid">
+					<div class="mdl-cell mdl--cell--2-col">
+						Anokol
+					</div>
+					<div class="mdl-cell mdl--cell--2-col">
+						Evangelista
+					</div>
+					<div class="mdl-cell mdl--cell--2-col">
+						Iquina
+					</div>
+				</div>
+			</div>
+		</main>
+
+		<footer class="mdl-mini-footer">
+			<div class="mdl-mini-footer--left-section">
+				<div class="mdl-logo">Wealth Management</div>
+				<ul class="mdl-mini-footer--link-list">
+					<li><a href="">Help</a></li>
+					<li><a href="">About us</a></li>
+				</ul>
+			</div>
+		</footer>
+	</div>
+
+</body>
+</html>
